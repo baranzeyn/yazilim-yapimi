564b9d50baa7fc6be352b8e603443504832f4f7e
baranzeyn <baranzeyn676@gmail.com>
2023-02-28 08:05:20 +0300

    update

--- yazilimYapimi/src/Main.java
+++ yazilimYapimi/src/Main.java
@@ -20,11 +20,12 @@ public class Main {
 	public static void main(String[] args) {
 		ArrayList<Integer> sayilar2 = new ArrayList<Integer>();
 		Scanner scan = new Scanner(System.in);
-		int hedef = (int) (Math.random() * 100);
+		int hedef = (int) (Math.random() * 1000);
+		sayilar2.add((int) (Math.random() * (90) + 10));
 		boolean isRun = true;
 		int sonuc = 0;
 		for (int i = 0; i < 5; i++) {
-			int a= (int) (Math.random() * 10);
+			int a = (int) (Math.random() * 10);
 			sayilar2.add(a);
 		}
 		System.out.println("hedef:" + hedef);
@@ -36,22 +37,24 @@ public class Main {
 			System.out.println("Kullanacağınız sayıların numaralarını giriniz:");
 			int secim1 = scan.nextInt();
 			int secim2 = scan.nextInt();
-			/* alınan değer index no+1 olduğu için -1li olan hali alınır*/
+			/* alınan değer index no+1 olduğu için -1li olan hali alınır */
 			int sayi1 = sayilar2.get(secim1 - 1);
 			int sayi2 = sayilar2.get(secim2 - 1);
-			
-			//kullanılacak sayılar arraylistten silinir
-			sayilar2.remove(secim1 - 1);//**
-			
-			if(secim2-1==0) {
-				sayilar2.remove(secim2 - 1);//zaten seçilen elemanın indexi sıfırsa bu azalmadan index numarası olarak etkilenmez
-			}else {
-				//yıldızlı işlem sonrası toplam index numaraları  bir azalır.Çünkü toplam eleman sayısı azalır.
+
+			// kullanılacak sayılar arraylistten silinir
+			sayilar2.remove(secim1 - 1);// **
+
+			if (secim2 - 1 == 0) {
+				sayilar2.remove(secim2 - 1);// zaten seçilen elemanın indexi sıfırsa bu azalmadan index numarası olarak
+											// etkilenmez
+			} else {
+				// yıldızlı işlem sonrası toplam index numaraları bir azalır.Çünkü toplam eleman
+				// sayısı azalır.
 				sayilar2.remove(secim2 - 2);
 			}
-			
-			
-			System.out.println("Yapmak istediğininiz işlemi seçiniz\n1-Toplama\n2-Çıkarma\n3-çarpma\n4-Bölme");
+
+			System.out.println(
+					"Yapmak istediğininiz işlemi seçiniz\n1-Toplama\n2-Çıkarma\n3-çarpma\n4-Bölme\n5-İşlem yapma");
 			int secim = scan.nextInt();
 			switch (secim) {
 			case 1:
@@ -75,15 +78,16 @@ public class Main {
 					System.out.println("sonuç hesaplanamaz");
 					System.exit(0);
 				}
+				break;
+			case 5:
+				System.out.println("İşlem dizisinden çıkılıyor...");
+				break;
 			}
-			
-			//elde edilen sonuc arrayliste eklenir
-			sayilar2.add(sonuc);
-			
-			System.out.println("İşlemi sonlandırmak istiyor musunuz?\n1-Evet\n2-Hayır");
-			int x = scan.nextInt();
 
-			if (x == 1) {
+			// elde edilen sonuc arrayliste eklenir
+			sayilar2.add(sonuc);
+
+			if (secim == 5) {
 				isRun = false;
 			}
 
